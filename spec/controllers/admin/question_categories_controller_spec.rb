b8ccda7f7244937c4d8cdbdd3c47fd767ed1b51b
Tobias Bohwalli <hi@futhr.io>
2014-06-14 12:00:31 +0200

    Dry specs using global require spec_helper.

--- spec/controllers/admin/question_categories_controller_spec.rb
+++ spec/controllers/admin/question_categories_controller_spec.rb
@@ -1,5 +1,3 @@
-require 'spec_helper'
-
 describe Spree::Admin::QuestionCategoriesController do
   stub_authorization!
 
--- spec/features/admin/faq_spec.rb
+++ spec/features/admin/faq_spec.rb
@@ -1,5 +1,3 @@
-require 'spec_helper'
-
 feature 'Admin FAQ', :js do
   stub_authorization!
 
