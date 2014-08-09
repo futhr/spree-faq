d2b56ea61e450fe32f0a84a4b930b4d65a1becc8
Tobias Bohwalli <hi@futhr.io>
2014-08-10 00:14:58 +0200

    Use nested module/class definitions.

--- app/models/spree/question_category.rb
+++ app/models/spree/question_category.rb
@@ -1,9 +1,11 @@
-class Spree::QuestionCategory < ActiveRecord::Base
-  acts_as_list
+module Spree
+  class QuestionCategory < ActiveRecord::Base
+    acts_as_list
 
-  has_many :questions, class_name: 'Spree::Question'
-  validates :name, presence: true
-  validates :name, uniqueness: { case_sensitive: false }
-  accepts_nested_attributes_for :questions, allow_destroy: true
-  attr_accessible :name, :questions_attributes, :question, :answer
+    has_many :questions, class_name: 'Spree::Question'
+    validates :name, presence: true
+    validates :name, uniqueness: { case_sensitive: false }
+    accepts_nested_attributes_for :questions, allow_destroy: true
+    attr_accessible :name, :questions_attributes, :question, :answer
+  end
 end
