f8dcbc7348f258a3ad99ea03bdad08a625b3cf1b
Tobias Bohwalli <hi@futhr.io>
2014-11-22 04:58:54 +0100

    Hook admin sidebar correctly.

--- app/overrides/add_faq_admin_tab.rb
+++ app/overrides/add_faq_admin_tab.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(virtual_path:  'spree/admin/shared/_configuration_menu',
                      name:          'add_faq_configuration_line',
-                     insert_bottom: '[data-hook="admin_configurations_sidebar_menu"]',
-                     text:          '<%= configurations_sidebar_menu_item Spree.t(:question_categories_admin, scope: :spree_faq), admin_question_categories_url %>',
+                     insert_bottom: "[data-hook='admin_configurations_sidebar_menu'], #admin_configurations_sidebar_menu[data-hook]",
+                     text:          '<%= configurations_sidebar_menu_item Spree.t(:question_categories_admin, scope: :spree_faq), admin_question_categories_path %>',
                      disabled:      false)
