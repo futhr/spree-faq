bf4a221b19acd65b6a37f73f20a73e29a3c645c3
Tobias Bohwalli <hi@futhr.io>
2014-12-05 14:14:01 +0100

    Now it only need assets for frontend.

--- lib/generators/spree_faq/install/install_generator.rb
+++ lib/generators/spree_faq/install/install_generator.rb
@@ -5,12 +5,10 @@ module SpreeFaq
 
       def add_javascripts
         append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/spree_faq\n"
-        append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/spree_faq\n"
       end
 
       def add_stylesheets
         inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_faq\n", before: /\*\//, verbose: true
-        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/spree_faq\n", before: /\*\//, verbose: true
       end
 
       def add_migrations
