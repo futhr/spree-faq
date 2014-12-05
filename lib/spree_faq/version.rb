e94ad67d0930255f26b1de752eddd42f825d6ffc
Tobias Bohwalli <hi@futhr.io>
2014-12-05 09:13:20 +0100

    Bump to v3.0.0.beta

--- lib/spree_faq/version.rb
+++ lib/spree_faq/version.rb
@@ -1,13 +1,15 @@
 module SpreeFaq
+  module_function
 
-  # Returns the version of the currently loaded SpreeFaq as a <tt>Gem::Version</tt>
-  def self.version
+  # Returns the version of the currently loaded SpreeFaq as a
+  # <tt>Gem::Version</tt>.
+  def version
     Gem::Version.new VERSION::STRING
   end
 
   module VERSION
-    MAJOR = 2
-    MINOR = 3
+    MAJOR = 3
+    MINOR = 0
     TINY  = 0
     PRE   = 'beta'
 
