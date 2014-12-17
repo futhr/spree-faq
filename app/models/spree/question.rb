25d9d3323ac0e85e714f60040b8016b372962e0e
Tobias Bohwalli <hi@futhr.io>
2014-12-17 20:21:40 +0100

    Ensure question unique within its category.

--- app/models/spree/question.rb
+++ app/models/spree/question.rb
@@ -3,8 +3,15 @@ module Spree
     acts_as_list
 
     belongs_to :question_category, touch: true, inverse_of: :questions
+
     validates :question_category_id, :question, :answer, presence: true
+    validates :question, uniqueness: {
+      scope: :question_category,
+      case_sensitive: false
+    }
+
     delegate :name, to: :question_category, prefix: true, allow_nil: true
+
     default_scope -> { order(position: :asc) }
   end
 end
--- app/models/spree/question_category.rb
+++ app/models/spree/question_category.rb
@@ -3,8 +3,10 @@ module Spree
     acts_as_list
 
     has_many :questions, -> { order(:position) }, dependent: :destroy, inverse_of: :question_category
+
     validates :name, presence: true
     validates :name, uniqueness: { case_sensitive: false }
+
     default_scope -> { order(position: :asc) }
   end
 end
