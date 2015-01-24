031f993df6b9cd7cb1498d1c2574e4ad7267ccfe
Tobias Bohwalli <hi@futhr.io>
2015-01-24 01:37:38 +0100

    Remove unused Deface option "disabled".

--- app/overrides/add_faq_to_admin_sidebar_menu.rb
+++ app/overrides/add_faq_to_admin_sidebar_menu.rb
@@ -2,6 +2,5 @@ Deface::Override.new(
   virtual_path:  'spree/layouts/admin',
   name:          'faq_admin_sidebar_menu',
   insert_bottom: '.sidebar-menu',
-  partial:       'spree/admin/shared/faq_main_menu',
-  disabled:      false
+  partial:       'spree/admin/shared/faq_main_menu'
 )
