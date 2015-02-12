b486f901a295212a8dffb31b42fc0c34af75dd66
Tobias Bohwalli <hi@futhr.io>
2015-02-12 05:03:37 +0100

    Fixes Rails 5 timestamp deprecation warning.

--- db/migrate/20090526213535_create_questions.rb
+++ db/migrate/20090526213535_create_questions.rb
@@ -1,16 +1,16 @@
 class CreateQuestions < ActiveRecord::Migration
-  def self.up
+  def up
     create_table :questions do |t|
       t.integer :question_category_id
       t.text    :question
       t.text    :answer
       t.integer :position
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 
-  def self.down
+  def down
     drop_table :questions
   end
 end
